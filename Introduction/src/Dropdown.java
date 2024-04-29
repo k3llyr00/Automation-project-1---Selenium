0beb2cbf6d005c5d96ab684725c338a34aaaac58
k3llyr00 <k3llyr00@gmail.com>
2024-04-29 23:19:34 +0300

    Alternative solution for dynamic (indexes) dropdown

--- Introduction/src/Dropdown.java
+++ Introduction/src/Dropdown.java
@@ -10,9 +10,16 @@ public class Dropdown {
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();
 		Thread.sleep(2000);
-		//without index [2] Selenium can't find the element, 
-		//because there's 2 matching elements and the first one is closed (because departure dropdown is closed
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+		// Solution 1: without index [2] Selenium can't find the element, 
+		//because there's 2 matching elements and the first one is closed (because departure drop-down is closed
+		
+		//therefore, hard coding indexes are not always preferred solution.
+		
+		// driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+		//Solution 2: //parent //child xpath following each other
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='MAA']")).click();
 		
 
 	}

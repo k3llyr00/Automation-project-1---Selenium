0d2db1b7e71141f5fbfa07034ff5dfa65c722bec
k3llyr00 <k3llyr00@gmail.com>
2024-05-06 12:19:36 +0300

    Working with checkboxes and assertions

--- Introduction/src/UpdatedDropdown.java
+++ Introduction/src/UpdatedDropdown.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+//TestNG testing framework will provide assertions.
+import org.testng.Assert;
 
 public class UpdatedDropdown {
 
@@ -10,19 +12,28 @@ public class UpdatedDropdown {
 		
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click(); // partial id
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6); //findElements in plural, otherwise its taking the first one and returning this
+		
 		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000);
 		
-		System.out.println(driver.findElement(By.id("divpaxinfo")).getText()); // Value of adult passengers before the loop
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "1 Adult");
 		
 		// click 4 times '+'-button
+		// int i = 1, because the default value is 1.
 		for (int i = 1; i < adultPassengers; i++) {
 			driver.findElement(By.id("hrefIncAdt")).click();
 		}
 		
 		driver.findElement(By.id("btnclosepaxoption")).click();
-		System.out.println(driver.findElement(By.id("divpaxinfo")).getText()); // Value of adult passengers after the loop
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 		
+		driver.close();
 	}
 
 }

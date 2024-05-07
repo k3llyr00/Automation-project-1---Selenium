054e9b89fc2e901112645529c632f8a400cd554d
k3llyr00 <k3llyr00@gmail.com>
2024-05-07 14:02:53 +0300

    removed not used imports from e2e test

--- Introduction/src/e2e.java
+++ Introduction/src/e2e.java
@@ -1,12 +1,8 @@
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class e2e {
